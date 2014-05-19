befa0c84ef046c42bdb0a551b769cac075adb806
phuong ngo <phuong.ngo@freerangetechnology.com>
2014-05-19 15:43:12 +0700

    add project to github

---
+++ build/Coveralls-iOS.build/Debug-iphonesimulator/Coveralls-iOS.build/Objects-normal/x86_64/Math.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ngohoaiphuong/sources/Code-Coverage/Coveralls-iOS/Coveralls-iOS/Math.m \
+  /Users/ngohoaiphuong/sources/Code-Coverage/Coveralls-iOS/Coveralls-iOS/Math.h
---
+++ build/Coveralls-iOS.build/Debug-iphonesimulator/Coveralls-iOSTests.build/Objects-normal/x86_64/NSArray+AL_firstObject.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ngohoaiphuong/sources/Code-Coverage/Coveralls-iOS/Coveralls-iOS/NSDArray-AL_firstObject/NSArray+AL_firstObject.m \
+  /Users/ngohoaiphuong/sources/Code-Coverage/Coveralls-iOS/Coveralls-iOS/NSDArray-AL_firstObject/NSArray+AL_firstObject.h
