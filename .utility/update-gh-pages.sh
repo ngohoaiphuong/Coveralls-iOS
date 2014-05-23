08ae57faf59e86c4a97883472fe778f584f7c620
phuong ngo <phuong.ngo@freerangetechnology.com>
2014-05-23 22:55:28 +0700

    continue edit and run coveralls -- 34000

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -2,22 +2,23 @@ export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
 echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BUILD_NUMBER = $TRAVIS_BUILD_NUMBER"
+echo "GH_TOKEN = $GH_TOKEN"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+# if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#   echo -e "Starting to update gh-pages\n"
 
-  cp -R report $HOME/coverage
+#   cp -R report $HOME/coverage
 
-  cd $HOME
-  git config --global user.email "ngohoai.phuong@gmail.com"
-  git config --global user.name "Travis"
+#   cd $HOME
+#   git config --global user.email "ngohoai.phuong@gmail.com"
+#   git config --global user.name "Travis"
 
-  cd gh-pages
-  cp -Rf $HOME/coverage/* .
+#   cd gh-pages
+#   cp -Rf $HOME/coverage/* .
 
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+#   git add -f .
+#   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+#   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Done magic with coverage\n"
-fi
+#   echo -e "Done magic with coverage\n"
+# fi
