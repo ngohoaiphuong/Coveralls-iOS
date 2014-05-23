4dde929e645ce6449998312820fca01e493b10af
phuong ngo <phuong.ngo@freerangetechnology.com>
2014-05-24 00:11:05 +0700

    test 9999000011111

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -4,14 +4,19 @@ echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BUILD_NUMBER = $TRAVIS_BUILD_NUMBER"
 echo "GH_TOKEN = ${GH_TOKEN}"
 
-# if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-#   echo -e "Starting to update gh-pages\n"
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  cd $HOME
+
+  git config --global user.email "ngohoai.phuong@gmail.com"
+  git config --global user.name "Travis"
+
+  git clone --depth=50 --branch=ci-report/template git://github.com/${TRAVIS_REPO_SLUG}.git $HOME/$TRAVIS_BUILD_NUMBER
+
 
 #   cp -R report $HOME/coverage
 
-#   cd $HOME
-#   git config --global user.email "ngohoai.phuong@gmail.com"
-#   git config --global user.name "Travis"
 
 #   cd gh-pages
 #   cp -Rf $HOME/coverage/* .
@@ -21,4 +26,4 @@ echo "GH_TOKEN = ${GH_TOKEN}"
 #   git push -fq origin gh-pages > /dev/null
 
 #   echo -e "Done magic with coverage\n"
-# fi
+fi
