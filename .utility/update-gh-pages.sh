93505151462dc2f40695e8e73d38295565242d30
phuong ngo <phuong.ngo@freerangetechnology.com>
2014-05-24 18:31:39 +0700

    test 999999999-0000000000

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -1,41 +1,41 @@
 export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
-GH_TOKEN = "14266bb42129ae71c1412dcf0d0623b46b580986"
+token="14266bb42129ae71c1412dcf0d0623b46b580986"
+gh_token=$1
 
 echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BUILD_NUMBER = $TRAVIS_BUILD_NUMBER"
-echo "GH_TOKEN = $GH_TOKEN"
+echo "GH_TOKEN = $gh_token"
+echo "User=$USERNANE"
 
-curl -X POST -u ngohoaiphuong -H "Content-Type: application/json" -d "{\"scopes\":[\"public_repo\"],\"note\":\"token for pushing from travis\"}" https://api.github.com/authorizations
+# if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#   echo -e "Starting to update gh-pages\n"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+#   cp -R report $HOME/report
+#   cd $HOME
 
-  cp -R report $HOME/report
-  cd $HOME
+#   git config --global user.email "ngohoai.phuong@gmail.com"
+#   git config --global user.name "Travis"
 
-  git config --global user.email "ngohoai.phuong@gmail.com"
-  git config --global user.name "Travis"
+#   git clone --depth=50 --branch=ci-report/template https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git $HOME/$TRAVIS_BUILD_NUMBER
 
-  git clone --depth=50 --branch=ci-report/template https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git $HOME/$TRAVIS_BUILD_NUMBER
+#   cd $TRAVIS_BUILD_NUMBER
 
-  cd $TRAVIS_BUILD_NUMBER
+#   git remote -v
 
-  git remote -v
+#   git remote add my_origin https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git
 
-  git remote add my_origin https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git
+#   git checkout -b ci-report/feature/build_$TRAVIS_BUILD_NUMBER
+#   git push -u my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
 
-  git checkout -b ci-report/feature/build_$TRAVIS_BUILD_NUMBER
-  git push -u my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
+#   cp -R $HOME/report/*/ .
 
-  cp -R $HOME/report/*/ .
+# #   cd gh-pages
+# #   cp -Rf $HOME/coverage/* .
 
-#   cd gh-pages
-#   cp -Rf $HOME/coverage/* .
+#   git add -f .
+#   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+#   git push -fq my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
 
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
-
-  echo -e "Done magic with coverage\n"
-fi
+#   echo -e "Done magic with coverage\n"
+# fi
