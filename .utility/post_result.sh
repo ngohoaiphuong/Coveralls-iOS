69106097831afc003dc71a0d4e58442d6587255c
phuong ngo <phuong.ngo@freerangetechnology.com>
2014-05-30 15:57:54 +0700

    edit travis.yml

--- .utility/post_result.sh
+++ .utility/post_result.sh
@@ -3,5 +3,6 @@ export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
 echo '-----------------------------'
 echo "REPOSITORY=$REPO"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
+echo "TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG"
 echo "GH_TOKEN=${GH_TOKEN}"
 echo '-----------------------------'
