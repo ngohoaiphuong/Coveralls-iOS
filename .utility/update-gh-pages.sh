46af30de4afc325faca031e386cab0b9a92ba944
phuong ngo <phuong.ngo@freerangetechnology.com>
2014-05-30 10:57:32 +0700

    enable put comment to github by curl

--- .utility/update-gh-pages.sh
+++ .utility/update-gh-pages.sh
@@ -8,39 +8,42 @@ echo "TRAVIS_BUILD_NUMBER = $TRAVIS_BUILD_NUMBER"
 echo "GH_TOKEN = $gh_token"
 list=`curl https://api.github.com/repos/ngohoaiphuong/Coveralls-iOS/pulls`
 echo '---------'
-echo $list
+# echo $list
 echo ">>>>>>GH_TOKEN=${GH_TOKEN}"
 
-# if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-#   echo -e "Starting to update gh-pages\n"
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
 
-#   cp -R report $HOME/report
-#   cd $HOME
+  cp -R report $HOME/report
+  cd $HOME
 
-#   git config --global user.email "ngohoai.phuong@gmail.com"
-#   git config --global user.name "Travis"
+  git config --global user.email "ngohoai.phuong@gmail.com"
+  git config --global user.name "Travis"
 
-#   git clone --depth=50 --branch=ci-report/template https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git $HOME/$TRAVIS_BUILD_NUMBER
+  git clone --depth=50 --branch=ci-report/template https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git $HOME/$TRAVIS_BUILD_NUMBER
 
-#   cd $TRAVIS_BUILD_NUMBER
+  cd $TRAVIS_BUILD_NUMBER
 
-#   git remote -v
+  git remote -v
 
-#   git remote add my_origin https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git
+  git remote add my_origin https://14266bb42129ae71c1412dcf0d0623b46b580986@github.com/${TRAVIS_REPO_SLUG}.git
 
-#   git checkout -b ci-report/feature/build_$TRAVIS_BUILD_NUMBER
-#   git push -u my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
+  git checkout -b ci-report/feature/build_$TRAVIS_BUILD_NUMBER
+  git push -u my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
 
-#   cp -R $HOME/report/*/ .
+  cp -R $HOME/report/*/ .
 
-# #   cd gh-pages
-# #   cp -Rf $HOME/coverage/* .
+#   cd gh-pages
+#   cp -Rf $HOME/coverage/* .
 
-#   git add -f .
-#   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-#   git push -fq my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq my_origin ci-report/feature/build_$TRAVIS_BUILD_NUMBER
 
-#   echo -e "Done magic with coverage\n"
-# fi
+  echo -e "Done magic with coverage\n"
+fi
 
+message_str="[Click here to view report](https://rawgit.com/ngohoaiphuong/Coveralls-iOS/ci-report/feature/build_$TRAVIS_BUILD_NUMBER/index.html)"
 # curl -X POST -d "{\"body\":\"test add comment\"}" -H 'Authorization: token 14266bb42129ae71c1412dcf0d0623b46b580986' https://api.github.com/repos/ngohoaiphuong/Coveralls-iOS/issues/2/comments
+curl -X POST -d "{\"body\":\"$message_str\"}" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/ngohoaiphuong/Coveralls-iOS/issues/2/comments
+
