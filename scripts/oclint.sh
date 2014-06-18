c8635fd803d1a3401160874d12f71ca1450158b8
vfa-phuongnh <vfa-phuongnh>
2014-06-18 12:30:41 +0700

    set path for oclint

--- scripts/oclint.sh
+++ scripts/oclint.sh
@@ -1,13 +1,14 @@
-# wget http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz &> /dev/null
+wget http://archives.oclint.org/nightly/oclint-0.9.dev.43e26f7-x86_64-darwin-12.4.0.tar.gz
+tar -xvzf oclint-0.9.dev.43e26f7-x86_64-darwin-12.4.0.tar.gz &> /dev/null
 
-wget http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz
-tar -xvjf oclint-0.7-x86_64-apple-darwin-10.tar.gz &> /dev/null
-
-if [[ ! -d oclint-0.7-x86_64-apple-darwin-10 ]]; then
-	#statements
+if [[ ! -d oclint-0.9.dev.43e26f7 ]]; then
 	echo "Can't install OClint tool, please try another time"
 	exit 1
 fi
 
-OCLINT_HOME=$TRAVIS_BUILD_DIR/oclint-0.7-x86_64-apple-darwin-10
+OCLINT_HOME=$TRAVIS_BUILD_DIR/oclint-0.9.dev.43e26f7
 export PATH=$OCLINT_HOME/bin:$PATH
+
+echo '---------------checking exists oclint--------------'
+which oclint
+echo '---------------------------------------------------'
